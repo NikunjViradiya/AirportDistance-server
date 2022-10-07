abde16d224f4db9accbbe3708f8dc52f4bede6cd
unknown <nikunj@lucentinnovation.co.in>
2022-10-07 15:50:18 +0530

    changed from params to query

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const app = express();
 app.use(cors());
 require("dotenv").config();
 
-app.use("/:key", (req, res) => {
+app.use("/get", (req, res) => {
 	try {
-		const key = req.params.key;
-		if (!req.params.key || req.params.key.length < 1) return res.status.send([]);
-		let data = airport.filter((item) => item.name.includes(key) || item.iata.includes(key));
+		const key = req.query.key;
+		if (!req.query.key || req.query.key.length < 1)
+			return res.status(200).send({
+				data: airport.slice(0, 100),
+			});
+		let data = airport.filter((item) => item.name.toLowerCase().includes(key.toLowerCase()) || item.iata.toLowerCase().includes(key.toLowerCase()));
 		return res.status(200).send({
 			data: data.slice(0, 40),
 		});
